aeed598966cc1e65b7290bf91a8658fc615602fd
Hritvik Soni <hritviksoni768@gmail.com>
2024-03-14 12:04:54 +0530

    added code base

---
+++ src/main/java/com/hritvik/OpenApiAssignmentKommunicate/OpenApiAssignmentKommunicateApplication.java
@@ -0,0 +1,13 @@
+package com.hritvik.OpenApiAssignmentKommunicate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiAssignmentKommunicateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiAssignmentKommunicateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hritvik/OpenApiAssignmentKommunicate/OpenApiAssignmentKommunicateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hritvik.OpenApiAssignmentKommunicate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiAssignmentKommunicateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
